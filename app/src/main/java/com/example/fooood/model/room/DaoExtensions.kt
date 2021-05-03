9551dcac602b399e66f0ed23f82edd58ae95571f
Cameron <cameron.fisher@absa.africa>
2021-05-03 14:14:17 +0200

    Added meal queries and also generic upsert queries.
    - DaoExtensions.kt: Added the ability to upsert single and lists.
    - IMealDao.kt: Added the ability to getAllByValue and getRandomMeals.

---
+++ app/src/main/java/com/example/fooood/model/room/DaoExtensions.kt
@@ -0,0 +1,23 @@
+package com.example.fooood.model.room
+
+suspend fun <T> IBaseDao<T>.upsert(entity: T, baseDao: IBaseDao<T>) {
+    val result = baseDao.insert(entity)
+    if (result == -1L) {
+        baseDao.update(entity)
+    }
+}
+
+suspend fun <T> IBaseDao<T>.upsert(entities: List<T>, baseDao: IBaseDao<T>) {
+    val results = baseDao.insert(entities)
+    val updateList: MutableList<T> = mutableListOf()
+
+    for(i in results.indices) {
+        if(results[i] == -1L) {
+            updateList.add(entities[i])
+        }
+    }
+
+    if(updateList.isNotEmpty()) {
+        baseDao.update(updateList)
+    }
+}
---
+++ app/src/main/java/com/example/fooood/model/room/IMealDao.kt
@@ -0,0 +1,15 @@
+package com.example.fooood.model.room
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.example.fooood.model.models.Meal
+
+@Dao
+interface IMealDao : IBaseDao<IMealDao> {
+
+    @Query("SELECT * FROM meal WHERE meal LIKE ('%'|:value|'%')")
+    suspend fun getAllByValue(value: String): List<Meal>?
+
+    @Query("SELECT * FROM meal ORDER BY RANDOM() LIMIT 100")
+    suspend fun getRandomMeals(): List<Meal>?
+}
