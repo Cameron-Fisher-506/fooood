31b5220495b695a62548d8b7120a8797c67474f6
Cameron <cameron.fisher@absa.africa>
2021-05-04 10:52:18 +0200

    Added navigation and bottom navigation view.
    - activity_main.xml: Added a navHostFragment.
    - bottom_nav.xml: Added a bottom navigation.
    - build.gradle: Added navigation safeargs. Set viewBinding. Added navigation dependencies.
    - build.gradle: Added navigation safeargs plugin.
    - colors.xml: Added dark blue.
    - home_navigation_graph.xml: Added navigation items.
    - drawable: Added icons.
    - meal_list_fragment.xml: Added a fragment to display list of meals.
    - MealListFragment.kt: Added a fragment to display list of meals.
    - strings.xml: Added menu strings.
    - themes.xml: Added bottom navigation theme.

---
+++ app/src/main/java/com/example/fooood/view/meal/MealListFragment.kt
@@ -0,0 +1,13 @@
+package com.example.fooood.view.meal
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.fooood.R
+
+class MealListFragment: Fragment(R.layout.meal_list_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
