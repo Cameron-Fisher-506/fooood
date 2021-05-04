abaa47811196db921ef5960b947c6545c2f1a54c
Cameron <cameron.fisher@absa.africa>
2021-05-04 11:02:49 +0200

    Setup navigation controller and bottom navigation view.
    - activity_main.xml: set fragment id.
    - MainActivity.kt: Setup navigation controller and bottom navigation view.

--- app/src/main/java/com/example/fooood/MainActivity.kt
+++ app/src/main/java/com/example/fooood/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.fooood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.fooood.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
+
+        wireUI()
+        attachNavController()
+    }
+
+    private fun wireUI() {
+        this.binding.bottomNavigationView.selectedItemId = R.id.home
+        this.binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+
+            }
+            true
+        }
+    }
+
+    private  fun attachNavController() {
+        this.navController = Navigation.findNavController(this, R.id.navHostFagment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(this.navController, null)
     }
 }
