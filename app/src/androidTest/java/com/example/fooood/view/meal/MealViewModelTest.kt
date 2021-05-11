5ca4530283f9d939ed4d6b6eebe662aca5f437bd
Cameron <cameron.fisher@absa.africa>
2021-05-11 13:56:48 +0200

    Added dependencies for unit testing.

---
+++ app/src/androidTest/java/com/example/fooood/view/meal/MealViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.example.fooood.view.meal
+
+import android.app.Application
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import org.junit.Before
+
+class MealViewModelTest {
+    lateinit var mealViewModel: MealViewModel
+
+    @Before
+    fun setup() {
+
+    }
+}
