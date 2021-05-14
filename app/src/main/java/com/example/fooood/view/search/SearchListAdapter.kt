257c0054e8837e3e0490b45669d7db36fe9cba42
Cameron <cameron.fisher@absa.africa>
2021-05-14 09:51:39 +0200

    Added a Meal list adapter.
    - search_navigation_graph.xml: Added fragments to the graph.
    - SearchListAdapter.kt: Added an adapter to display the meals.
    - SearchListFragment.kt: Added layout.

---
+++ app/src/main/java/com/example/fooood/view/search/SearchListAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.fooood.view.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.fooood.databinding.MealItemBinding
+import com.example.fooood.model.models.Meal
+
+class SearchListAdapter(private val meals: ArrayList<Meal>) :
+    RecyclerView.Adapter<SearchListAdapter.ViewHolder>() {
+    class ViewHolder(val binding: MealItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = MealItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Glide.with(holder.binding.root)
+            .asBitmap()
+            .load(meals[position].mealThumb)
+            .into(holder.binding.mealImageView)
+
+        holder.binding.foodNameTextView.text = meals[position].meal
+
+        holder.binding.mealConstraintLayout.setOnClickListener {
+
+        }
+    }
+
+    override fun getItemCount() = meals.size
+
+    fun updateMealList(meals: List<Meal>) {
+        this.meals.clear()
+        this.meals.addAll(meals)
+        this.notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/example/fooood/view/search/SearchListFragment.kt
@@ -0,0 +1,13 @@
+package com.example.fooood.view.search
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.fooood.R
+
+class SearchListFragment : Fragment(R.layout.search_list_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
