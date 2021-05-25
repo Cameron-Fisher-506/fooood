aa8c4ab53002fadbf94a0b175bdc572dff22a649
Cameron <cameron.fisher@absa.africa>
2021-05-25 13:31:16 +0200

    Fixed room database bugs.
    - Category.kt: Added primary key.

--- app/src/main/java/com/example/fooood/model/models/Category.kt
+++ app/src/main/java/com/example/fooood/model/models/Category.kt
@@ -2,12 +2,15 @@ package com.example.fooood.model.models
 
 import androidx.room.Entity
 import androidx.room.Index
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 @Entity(indices = [Index(value = ["id"], unique = true)])
-class Category: BaseModel() {
+class Category: BaseModel(), Serializable {
     var bookId: Int = 1
     @SerializedName("idCategory")
+    @PrimaryKey(autoGenerate = false)
     var id: String = ""
     @SerializedName("strCategory")
     var category: String = ""
