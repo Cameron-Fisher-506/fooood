757204b393c84ce7a87472a5f0e1e87a9bd67ebf
Cameron <cameron.fisher@absa.africa>
2021-06-15 13:48:33 +0200

    Setup the switch for dark mode.

--- app/src/main/java/com/example/fooood/view/menu/app/AppFragment.kt
+++ app/src/main/java/com/example/fooood/view/menu/app/AppFragment.kt
@@ -2,6 +2,7 @@ package com.example.fooood.view.menu.app
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.example.fooood.R
 import com.example.fooood.databinding.AppFragmentBinding
@@ -12,5 +13,17 @@ class AppFragment : Fragment(R.layout.app_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         this.binding = AppFragmentBinding.bind(view)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        this.binding.darkModeSwitch.setOnCheckedChangeListener {view, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 }
